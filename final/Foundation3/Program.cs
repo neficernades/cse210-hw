bce982756757930684742d859bc9ee7538450906
neficernades <neficernades@gmail.com>
2024-06-06 00:01:04 -0500

    yep

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,128 @@
 using System;
+using System.Collections.Generic;
+
+class Address
+{
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+    public string Country { get; private set; }
+
+    public Address(string street, string city, string state, string country)
+    {
+        Street = street;
+        City = city;
+        State = state;
+        Country = country;
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{Street}, {City}, {State}, {Country}";
+    }
+}
+
+class Event
+{
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public string Date { get; private set; }
+    public string Time { get; private set; }
+    public Address Address { get; private set; }
+
+    public Event(string title, string description, string date, string time, Address address)
+    {
+        Title = title;
+        Description = description;
+        Date = date;
+        Time = time;
+        Address = address;
+    }
+
+    public string GetStandardDetails()
+    {
+        return $"Title: {Title}\nDescription: {Description}\nDate: {Date}\nTime: {Time}\nAddress: {Address.GetFullAddress()}";
+    }
+
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails();
+    }
+
+    public string GetShortDescription()
+    {
+        return $"Type: {GetType().Name}\nTitle: {Title}\nDate: {Date}";
+    }
+}
+
+class Lecture : Event
+{
+    public string Speaker { get; private set; }
+    public int Capacity { get; private set; }
+
+    public Lecture(string title, string description, string date, string time, Address address, string speaker, int capacity)
+        : base(title, description, date, time, address)
+    {
+        Speaker = speaker;
+        Capacity = capacity;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{GetStandardDetails()}\nSpeaker: {Speaker}\nCapacity: {Capacity}";
+    }
+}
+
+class Reception : Event
+{
+    public string RsvpEmail { get; private set; }
+
+    public Reception(string title, string description, string date, string time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        RsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{GetStandardDetails()}\nRSVP Email: {RsvpEmail}";
+    }
+}
+
+class OutdoorGathering : Event
+{
+    public string Weather { get; private set; }
+
+    public OutdoorGathering(string title, string description, string date, string time, Address address, string weather)
+        : base(title, description, date, time, address)
+    {
+        Weather = weather;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{GetStandardDetails()}\nWeather: {Weather}";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        var address = new Address("789 Pine St", "Metropolis", "NY", "USA");
+
+        var lecture = new Lecture("Python Programming", "Learn Python", "2023-07-21", "10:00 AM", address, "Dr. Smith", 100);
+        var reception = new Reception("Company Meet and Greet", "Annual event", "2023-08-15", "6:00 PM", address, "rsvp@company.com");
+        var outdoor = new OutdoorGathering("Summer Picnic", "Join us for a fun day", "2023-06-30", "1:00 PM", address, "Sunny");
+
+        var events = new List<Event> { lecture, reception, outdoor };
+
+        foreach (var evt in events)
+        {
+            Console.WriteLine(evt.GetStandardDetails());
+            Console.WriteLine(evt.GetFullDetails());
+            Console.WriteLine(evt.GetShortDescription());
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,116 @@
 using System;
+using System.Collections.Generic;
+
+abstract class Activity
+{
+    public string Date { get; private set; }
+    public int Length { get; private set; } // in minutes
+
+    protected Activity(string date, int length)
+    {
+        Date = date;
+        Length = length;
+    }
+
+    public abstract float GetDistance(); // in miles
+    public abstract float GetSpeed(); // in mph
+    public abstract float GetPace(); // in minutes per mile
+
+    public string GetSummary()
+    {
+        return $"{Date} {GetType().Name} ({Length} min) - Distance: {GetDistance():0.00} miles, Speed: {GetSpeed():0.00} mph, Pace: {GetPace():0.00} min/mile";
+    }
+}
+
+class Running : Activity
+{
+    private float Distance { get; set; } // in miles
+
+    public Running(string date, int length, float distance) : base(date, length)
+    {
+        Distance = distance;
+    }
+
+    public override float GetDistance()
+    {
+        return Distance;
+    }
+
+    public override float GetSpeed()
+    {
+        return (Distance / Length) * 60; // miles per hour
+    }
+
+    public override float GetPace()
+    {
+        return Length / Distance; // minutes per mile
+    }
+}
+
+class Cycling : Activity
+{
+    private float Speed { get; set; } // in mph
+
+    public Cycling(string date, int length, float speed) : base(date, length)
+    {
+        Speed = speed;
+    }
+
+    public override float GetDistance()
+    {
+        return (Speed * Length) / 60; // distance in miles
+    }
+
+    public override float GetSpeed()
+    {
+        return Speed;
+    }
+
+    public override float GetPace()
+    {
+        return 60 / Speed; // minutes per mile
+    }
+}
+
+class Swimming : Activity
+{
+    private int Laps { get; set; }
+    private const float LapLength = 50; // length of each lap in meters
+
+    public Swimming(string date, int length, int laps) : base(date, length)
+    {
+        Laps = laps;
+    }
+
+    public override float GetDistance()
+    {
+        return (Laps * LapLength) / 1000 * 0.62f; // convert meters to miles
+    }
+
+    public override float GetSpeed()
+    {
+        return (GetDistance() / Length) * 60; // miles per hour
+    }
+
+    public override float GetPace()
+    {
+        return Length / GetDistance(); // minutes per mile
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        var running = new Running("2023-07-10", 30, 3); // 3 miles in 30 minutes
+        var cycling = new Cycling("2023-07-11", 45, 15); // 15 mph for 45 minutes
+        var swimming = new Swimming("2023-07-12", 30, 20); // 20 laps in 30 minutes
+
+        var activities = new List<Activity> { running, cycling, swimming };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
